f5a64462fe398459bbd159a1170086eeebde7a53
Honey-Joe <honeyjoe943@gmail.com>
2024-10-23 12:23:17 +0530

    dsfdfs

--- src/components/Header/VideoBackground.jsx
+++ src/components/Header/VideoBackground.jsx
@@ -7,7 +7,7 @@ const VideoBackground = () => {
       muted
       loop
       autoPlay
-      style={{ width: '100%', height: '100%', objectFit: 'fill' }}
+      style={{ width: '100%', height: '100%', objectFit: 'cover' }}
       src="https://raw.githubusercontent.com/Honey-Joe/video/main/uploads/3129576-sd_960_540_30fps.mp4"
 
       className='absolute z-0'
