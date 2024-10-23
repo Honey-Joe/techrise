840faff2f01092c94d7c3063896cd25b73f9cae2
Honey-Joe <honeyjoe943@gmail.com>
2024-10-23 13:17:02 +0530

    sdf

--- src/components/Eventslist/Eventlist.jsx
+++ src/components/Eventslist/Eventlist.jsx
@@ -6,32 +6,21 @@ const Eventlist = () => {
   return (
     <>
       <div className="max-w-[100%] bg-[#08123B]">
-        <div className="w-[90%] mx-auto grid grid-cols-1 py-10 gap-3">
+        <div className="w-[80%] mx-auto grid grid-cols-1 py-10 gap-5">
           <div className="flex justify-center">
             <div className="">
               <h5 class="flex  items-center justify-center gap-3">
-                <p className="font-[Poppins] text-[18px]  md:text-[22px] lg:text-[24px] font-semibold text-[#4f5de4] ">
-                  Latest Blog
+                <p className="font-[Poppins] text-[32px]  md:text-[22px] lg:text-[24px] font-semibold text-white ">
+                  Events
                 </p>
-                <svg
-                  class="arrow-svg"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 55 13"
-                  className="w-[60px] fill-[#F57104]"
-                >
-                  <g clip-path="url(#clip0_324_36194)">
-                    <path d="M10.5406 6.49995L0.700562 12.1799V8.56995L4.29056 6.49995L0.700562 4.42995V0.819946L10.5406 6.49995Z"></path>
-                    <path d="M25.1706 6.49995L15.3306 12.1799V8.56995L18.9206 6.49995L15.3306 4.42995V0.819946L25.1706 6.49995Z"></path>
-                    <path d="M39.7906 6.49995L29.9506 12.1799V8.56995L33.5406 6.49995L29.9506 4.42995V0.819946L39.7906 6.49995Z"></path>
-                    <path d="M54.4206 6.49995L44.5806 12.1799V8.56995L48.1706 6.49995L44.5806 4.42995V0.819946L54.4206 6.49995Z"></path>
-                  </g>
-                </svg>
               </h5>
             </div>
           </div>
           <div className="flex justify-center">
-            <p className="font-[Poppins] text-[22px]  md:text-[32px] lg:text-[40px] text-[#2a254d] font-semibold">Latest Updates & Articles</p>
+            <p className="font-[Poppins] text-[24px]  md:text-[32px] lg:text-[40px] text-white font-semibold">Technical Events</p>
           </div>
+          <div className="h-[3px] bg-[#118AEF] w-[90%] lg:w-[40%] mx-auto my-5 lg:my-10"></div>
+
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
             {
                 Eventlistdata.map((e)=>{
