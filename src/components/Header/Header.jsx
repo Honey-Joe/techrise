ecaca3a7a50987cac8f59f6ed8db4dbcc10cba3a
Honey-Joe <honeyjoe943@gmail.com>
2024-10-23 12:20:48 +0530

    df

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -45,7 +45,7 @@ const Header = () => {
           </div>
           
         </div>
-        <div className="w-[95%] lg:w-[70%] justify-center mx-auto gap-10 lg:gap-0 items-center grid grid-cols-1 lg:py-[0px] pt-[100px]">
+        <div className="w-[95%] lg:w-[70%] justify-center mx-auto gap-10 lg:gap-0 items-center grid grid-cols-1 lg:py-[0px] pt-[80px]">
           <div className="text-white font-bold text-center">
             <p className="font-[Fredoka] text-[26px] lg:text-[52px] text-center font-bold text-[#fbe072]">
               DEPARTMENT OF COMPUTER SCIENCE
