e9196ab2e8c8e0355b0cf32302890c200b6ab025
Honey Joe <honeyjoe942@gmail.com>
2024-11-14 22:55:46 +0530

    ds

--- src/components/EventDetails/EventDeatailsCard.jsx
+++ src/components/EventDetails/EventDeatailsCard.jsx
@@ -22,7 +22,7 @@ const EventDeatailsCard = (props) => {
             <img src="Asset/brochure.jpeg" class="rounded-lg w-full h-64 sm:h-80 md:h-96 object-cover shadow-lg" alt="Event Brochure" />
         </div>
         <div class="w-full md:w-6/12 p-4 mt-6 md:mt-0 text-center md:text-left">
-            <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold font-Fredoka pulse bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">{Eventdata}</h1>
+            <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold font-Fredoka pulse bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">{Eventdata.eventid}</h1>
             <h2 class="text-2xl sm:text-3xl mt-4 font-Fredoka">Debugging</h2>
             <p class="text-md sm:text-lg mt-6 leading-relaxed font-Fredoka">
                 {props.eventdesc}
