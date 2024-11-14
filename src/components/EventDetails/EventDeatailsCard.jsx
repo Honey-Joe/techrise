295a43db3c2df8b651264404b82c997261ee8ec3
Honey Joe <honeyjoe942@gmail.com>
2024-11-14 23:03:26 +0530

    aswd

--- src/components/EventDetails/EventDeatailsCard.jsx
+++ src/components/EventDetails/EventDeatailsCard.jsx
@@ -4,7 +4,17 @@ import axios from 'axios'
 
 const EventDeatailsCard = (props) => {
     
-   
+    const [Eventdata, setEventData] = useState();
+
+    const fetechData = async() =>{
+        const res = await axios.get("https://backendtest-nu.vercel.app/event");
+        setEventData(res.data);
+        console.log(res.data);
+
+    }
+    useEffect(()=>{
+        fetechData();
+    },[]);
   return (
     <div>
         <div class="flex flex-wrap justify-center py-8 px-4 bg-[#08123B]">
@@ -12,7 +22,7 @@ const EventDeatailsCard = (props) => {
             <img src="Asset/brochure.jpeg" class="rounded-lg w-full h-64 sm:h-80 md:h-96 object-cover shadow-lg" alt="Event Brochure" />
         </div>
         <div class="w-full md:w-6/12 p-4 mt-6 md:mt-0 text-center md:text-left">
-            <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold font-Fredoka pulse bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">{Eventdata.eventid}</h1>
+            <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold font-Fredoka pulse bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">{}=</h1>
             <h2 class="text-2xl sm:text-3xl mt-4 font-Fredoka">Debugging</h2>
             <p class="text-md sm:text-lg mt-6 leading-relaxed font-Fredoka">
                 {props.eventdesc}
