7c64ba77279aa3dd871f7ba370b3f458d58fbbe7
Honey-Joe <honeyjoe943@gmail.com>
2024-11-15 12:45:15 +0530

    assd

--- src/components/EventDetails/EventDeatailsCard.jsx
+++ src/components/EventDetails/EventDeatailsCard.jsx
@@ -7,22 +7,22 @@ const EventDeatailsCard = (props) => {
     
   return (
     <div>
-            {props.eventname}
+            
 
-        {/* <div className="flex flex-wrap justify-center py-8 px-4 bg-[#08123B]">
+        <div className="flex flex-wrap justify-center py-8 px-4 bg-[#08123B]">
         <div className="w-full md:w-5/12 p-4 transition duration-500 ease-in-out transform hover:scale-105">
             <img src="Asset/brochure.jpeg" className="rounded-lg w-full h-64 sm:h-80 md:h-96 object-cover shadow-lg" alt="Event Brochure" />
         </div>
         <div className="w-full md:w-6/12 p-4 mt-6 md:mt-0 text-center md:text-left">
-            <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold font-Fredoka pulse bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+            <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold font-Fredoka pulse bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">{props.eventname}
                 </h1>
             <h2 className="text-2xl sm:text-3xl mt-4 font-Fredoka">Debugging</h2>
-            <p className="text-md sm:text-lg mt-6 leading-relaxed font-Fredoka">
+            <p className="text-md sm:text-lg mt-6 leading-relaxed font-Fredoka">{props.eventdesc}
             </p>
         </div>
-    </div> */}
+    </div>
 
-    {/* <div className="my-16 text-center px-4">
+    <div className="my-16 text-center px-4">
         <h3 className="text-3xl sm:text-4xl font-bold font-Lugrasimo mb-12 pulse bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Meet the Coordinators</h3>
         <div className="flex flex-wrap justify-center">
             <div className="w-40 sm:w-48 lg:w-52 p-5 mx-3 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg">
@@ -41,7 +41,7 @@ const EventDeatailsCard = (props) => {
                 <p className="text-center text-sm font-Fredoka">6379420842</p>
             </div>
         </div>
-    </div> */}
+    </div>
     </div>
   )
 }
