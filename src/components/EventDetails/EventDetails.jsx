bcb0c9ba2699c474e7b6224384f2955cc31efc84
Honey-Joe <honeyjoe943@gmail.com>
2024-11-16 21:34:14 +0530

    ds

--- src/components/EventDetails/EventDetails.jsx
+++ src/components/EventDetails/EventDetails.jsx
@@ -8,12 +8,13 @@ const EventDetails = () => {
     const datap = useParams();
     console.log(datap.eventid);
 
-    const [data,setData] = useState();
+    const [data,setData] = useState([{}]);
 
   const fetchdata = async()=>{
     const res = await axios.get("https://techx-24backend.vercel.app/event/" + datap.eventid);
     setData(res.data)
     console.log(res.data);
+    console.log(data);
   }
  
   useEffect(()=>{
@@ -22,7 +23,13 @@ const EventDetails = () => {
   return (
     <div>
         <p>event Details</p>
-        <p>{data}</p>
+        <p>{data.map((e)=>{
+          return(
+            <>
+            <p>{e.eventid}</p>
+            </>
+          )
+        })}</p>
     </div>
   )
 }
